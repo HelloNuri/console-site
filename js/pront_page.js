a46933d04c8989f288f8f1b9271e7199fc7bed6a
HelloNuri <HelloNuri.dvl@gmail.com>
2017-12-15 01:40:58 +0900

    layout choi juck hwa & add memo function

---
+++ js/pront_page.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $('#content1').fadeOut('0');
+  $('#content1').fadeIn('3000');
+  $('#content2').fadeOut('0');
+  $('#content2').fadeIn('3000');
+  $('#content3').fadeOut('0');
+  $('#content3').fadeIn('3000');
+});
